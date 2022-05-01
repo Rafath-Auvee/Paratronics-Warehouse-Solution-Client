7a0d669b4904abc2226f034239b6c81c1dc6e3d2
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-05-01 19:59:54 +0600

    firebase config

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+
+import { initializeApp,getAuth } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQIMgZTuG-1CISerMuf0q8PS2qfU444iw",
+  authDomain: "paratronics-2213d.firebaseapp.com",
+  projectId: "paratronics-2213d",
+  storageBucket: "paratronics-2213d.appspot.com",
+  messagingSenderId: "423397072694",
+  appId: "1:423397072694:web:ff18abab172fe853925d97"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth
