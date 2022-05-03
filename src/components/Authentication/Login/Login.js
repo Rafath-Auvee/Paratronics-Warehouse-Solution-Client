59b382956cce12b1520afa338be2ac2d519233d5
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-05-03 06:45:55 +0600

    login system

--- src/components/Authentication/Login/Login.js
+++ src/components/Authentication/Login/Login.js
@@ -1,27 +1,34 @@
-import React, { useState } from "react";
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  signOut,
-  useCreateUserWithEmailAndPassword,
-} from "firebase/auth";
-
-import { useAuthState,  } from "react-firebase-hooks/auth";
-
-import auth from "../../../firebase.init.js";
-import {toast } from 'react-toastify';
-
+import React, { useState } from 'react';
+import { useLocation, useNavigate,Link} from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init.js';
+import { toast } from 'react-toastify';
+import Spinner from 'react-bootstrap/Spinner'
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  // const [createUserWithEmailAndPassword, user, loading, error] =
-  //   useCreateUserWithEmailAndPassword(auth);
-  const notify = () => toast("Wow so easy !");
-
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth);
+  if (error) {
+    return toast(`Error: ${error.message}`
+    );
+  }
+  if (loading) {
+    return <Spinner animation="grow" variant="dark"/>
+  }
+  if (user) {
+    return toast(`Welcome ${user.email}`)
+    
+  }
   return (
-    <div>Login</div>
-    // <button onClick={notify}>Notify !</button>
+    <div>
+      Login
+    </div>
   );
 };
 
